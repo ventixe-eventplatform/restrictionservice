806a8bbb093e01c814443774de5e0581e8be540e
Amanda Hult <amandahult95@gmail.com>
2025-06-07 15:22:36 +0200

    .

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -11,11 +11,12 @@ builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configura
 
 builder.Services.AddCors(x =>
 {
-    x.AddPolicy("AllowAll", x =>
+    x.AddPolicy("AllowFrontend", x =>
     {
-        x.AllowAnyMethod();
-        x.AllowAnyOrigin();
-        x.AllowAnyHeader();
+        x.WithOrigins("https://https://icy-pebble-08fddfa03.6.azurestaticapps.net")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
     });
 });
 
@@ -23,7 +24,7 @@ var app = builder.Build();
 
 app.MapOpenApi();
 app.UseHttpsRedirection();
-app.UseCors("AllowAll");
+app.UseCors("AllowFrontend");
 
 app.UseAuthentication();
 app.UseAuthorization();
